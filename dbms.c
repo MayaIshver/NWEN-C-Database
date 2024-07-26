22fb48e8ba6a30030cce7a3f4713828514051fdf
MayaIshver <157539535+MayaIshver@users.noreply.github.com>
2024-07-27 10:46:51 +1200

    Part 3
    I planned out and implemented the final part of the assignment (part 3). This is the remove row function which needed to do multiple things, including changing the total size of the database based on how many rows were used and moving all rows after the deleted one up in the database.

--- dbms.c
+++ dbms.c
@@ -38,6 +38,8 @@ int db_add_row(struct db_table *db, struct album *a){
 		int size = db->rows_total +5;
 		//reallocate mem (add 5)
 		db->table = realloc(db->table, size*sizeof(struct album));
+		//if i use size will it change data type
+		db->rows_total = db->rows_total+5;
 		//if mem is null there was not enough space
 		if(db->table == NULL){
 			return 0;
@@ -46,11 +48,76 @@ int db_add_row(struct db_table *db, struct album *a){
 
 	//store in rows used +1 
 	//add the row 
+	//need to equal row amount cause index starts from 0
 	int row = db->rows_used;
 	db->table[row] = *a;
+
+	
 	db->rows_used = row+1;
+	db_show_row(db, row);
 	return 1;
 }
 
+int db_remove_row(struct db_table *db, unsigned long id){
+	//to get album 
+	//for every row in the table 
+	//int rows = db -> rows_used;
+	int success = 0;
+	int index = -1;
+	//struct album *rowData = malloc(1*sizeof(struct album));
+	for(unsigned int i = 0; i<db -> rows_used; i++){
 
+		//access album id = table[1][0]??
+		struct album rowData = db -> table[i];
+		unsigned long albumId =  rowData.id;
+		printf("%lu\n",albumId);
+		//if id are equal
+		if(albumId == id){
+			index = i;
+			success = 1;
+			break;
+		}
+		//free(rowData);
+
+	}
+
+	//delete that row - do it by moving all the other ones up 
+
+	//for all table[i] after row 
+	if(index >= 0){
+		for(int j = index; j<db -> rows_used; j++){
+			db->table[j] = db->table[j+1];
+			//make table[i] = table[i+1] - moves all up 
+		}
+		db -> rows_used = db -> rows_used - 1;
+	}
+	
+			
+	//records after need to be moved up 
+	//if rowtotal-rowsused = 5 
+	int totalRows = db -> rows_total;
+	if(totalRows - (db -> rows_used) == 5){
+		//free 5 rows of memory 
+		
+		db->table = realloc(db->table, (totalRows-5)*sizeof(struct album));
+		if(db == NULL){
+			return 0;
+		}
+		//update total 
+		db -> rows_total = totalRows-5;
+	}
+	//update used rows 
+
+	//if rowused = 0 (or 1 if before updating)
+	if(db -> rows_used == 0){
+		//free whole array 
+		free(db);
+		//rows = 0 
+		db->rows_total;
+		db->rows_used;
+		//table = null
+		db->table = NULL;
+	}
+	return success; 	
+}
 
